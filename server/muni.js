7cb5d5be96aa07426807ba8c3f16916846c67532
Ryan James <ryanwilsonjames@gmail.com>
2016-12-09 20:16:41 -0800

    Remove cruft, filter vehicles for distance within 400m

--- server/muni.js
+++ server/muni.js
@@ -1,11 +1,10 @@
-const http = require('http');
-const fs = require('fs');
 const request = require('request-promise');
 
 const API_KEYS = require('../secrets');
 const MUNI_HELPERS = require('./utils/muni');
 const muniURL = `http://api.511.org/transit/vehiclemonitoring?api_key=${API_KEYS.API_KEY_511}&format=json&agency=sf-muni`;
-
+// This is in meters
+const nearbyDistance = 400;
 
 function getMuniData(req, res) {
   if (!req.body || !req.body.lat || !req.body.long) {
@@ -22,7 +21,7 @@ function getMuniData(req, res) {
     const sanitizedResponse = MUNI_HELPERS.stupidMuniAPIWorkaround(response);
     const parsedJson = JSON.parse(sanitizedResponse);
     const processedVehicles = MUNI_HELPERS.processMuniResponse(parsedJson, userLatLong);
-    const closestFiveVehicles = processedVehicles.slice(0, 5);
+    const closestFiveVehicles = processedVehicles.slice(0, 5).filter((vehicle) => vehicle.distance < nearbyDistance);
 
     res.json({ closestFiveVehicles }).end();
   });
