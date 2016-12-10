c62a99b6c2af0870dd95bd38e66cc2ba6b618fc3
Ryan James <ryanwilsonjames@gmail.com>
2016-12-10 14:10:40 -0800

    Move App out of views

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import store from './store';
-import AppView from './views/App';
+import AppView from './App';
 import './stylesheets/index.css';
 
 const target = document.getElementById('root');
