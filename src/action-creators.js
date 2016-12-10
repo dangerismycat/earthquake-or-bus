eb80530e6bad68a804fe18ccea17a87c21d3c19a
Ryan James <ryanwilsonjames@gmail.com>
2016-12-10 14:08:57 -0800

    Consolidate thunks

--- src/action-creators.js
+++ src/action-creators.js
@@ -6,7 +6,6 @@ const dataURLs = {
   usgs: '/api/usgs',
 };
 
-
 export function updateAttribute(attribute) {
   return {
     type: ACTIONS.UPDATE_ATTRIBUTE,
@@ -23,7 +22,7 @@ export function updateCurrentView(view) {
 
 
 // ASYNC THUNKS
-export function getNearbyVehiclesThunk({ lat, long }) {
+export function getNearbyDataThunk({ lat, long }) {
   return (dispatch) => {
     if (!isInSF({ lat, long })) {
       dispatch(updateAttribute({ locationsError: LOCATION_ERRORS.OUTSIDE_SF }));
@@ -33,49 +32,48 @@ export function getNearbyVehiclesThunk({ lat, long }) {
 
     console.log('fetching...')
     console.log(`Lat: ${lat}, Long: ${long}`);
-    fetch(dataURLs.muni, {
-      method: 'POST',
-      body: JSON.stringify({ lat, long }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-    .then((response) => response.json().then((json) => {
-      console.log('got some Muni data:', json)
-      const { closestFiveVehicles } = json;
-      dispatch(updateAttribute({ closestFiveVehicles }));
-    })
-    .catch((error) => {
-      // TODO: better error handling
-      console.error(`Error from server: ${error}`);
-    }));
+    getMuniData({ lat, long }, dispatch);
+    getUSGSData({ lat, long }, dispatch);
   };
 }
 
-export function getRecentEarthquakesThunk({ lat, long }) {
-  return (dispatch) => {
-    // TODO: consolidate this check with other thunk
-    if (!isInSF({ lat, long })) {
-      dispatch(updateAttribute({ locationsError: LOCATION_ERRORS.OUTSIDE_SF }));
-      dispatch(updateCurrentView(VIEW_FLOW.LOCATION_WARNING));
-      return;
-    }
 
-    fetch(dataURLs.usgs, {
-      method: 'POST',
-      body: JSON.stringify({ lat, long }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-    .then((response) => response.json().then((json) => {
-      console.log('got some USGS data:', json)
-      const { recentNearbyEarthquakes } = json;
-      dispatch(updateAttribute({ recentNearbyEarthquakes }));
-    })
-    .catch((error) => {
-      // TODO: better error handling
-      console.error(`Error from server: ${error}`);
-    }));
-  };
+// HELPERS
+
+function getMuniData({ lat, long }, dispatch) {
+  fetch(dataURLs.muni, {
+    method: 'POST',
+    body: JSON.stringify({ lat, long }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  .then((response) => response.json().then((json) => {
+    console.log('got some Muni data:', json)
+    const { closestFiveVehicles } = json;
+    dispatch(updateAttribute({ closestFiveVehicles }));
+  })
+  .catch((error) => {
+    // TODO: better error handling
+    console.error(`Error from server: ${error}`);
+  }));
+}
+
+function getUSGSData({ lat, long }, dispatch) {
+  fetch(dataURLs.usgs, {
+    method: 'POST',
+    body: JSON.stringify({ lat, long }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  .then((response) => response.json().then((json) => {
+    console.log('got some USGS data:', json)
+    const { recentNearbyEarthquakes } = json;
+    dispatch(updateAttribute({ recentNearbyEarthquakes }));
+  })
+  .catch((error) => {
+    // TODO: better error handling
+    console.error(`Error from server: ${error}`);
+  }));
 }
