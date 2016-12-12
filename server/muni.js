4a3a49b23e6ce91bdec74c2c855ff3c452fe995d
Ryan James <ryanwilsonjames@gmail.com>
2016-12-11 21:21:15 -0800

    No really, give units in miles/feet

--- server/muni.js
+++ server/muni.js
@@ -3,8 +3,8 @@ const request = require('request-promise');
 const API_KEYS = require('../secrets');
 const MUNI_HELPERS = require('./utils/muni');
 const muniURL = `http://api.511.org/transit/vehiclemonitoring?api_key=${API_KEYS.API_KEY_511}&format=json&agency=sf-muni`;
-// This is in meters
-const nearbyDistance = 400;
+// This is in feet
+const nearbyDistance = 1300;
 
 function getMuniData(req, res) {
   if (!req.body || !req.body.lat || !req.body.long) {
@@ -21,7 +21,8 @@ function getMuniData(req, res) {
     const sanitizedResponse = MUNI_HELPERS.stupidMuniAPIWorkaround(response);
     const parsedJson = JSON.parse(sanitizedResponse);
     const processedVehicles = MUNI_HELPERS.processMuniResponse(parsedJson, userLatLong);
-    const closestFiveVehicles = processedVehicles.slice(0, 5).filter((vehicle) => vehicle.distance < nearbyDistance);
+    const closestFiveVehicles =
+      processedVehicles.filter((vehicle) => vehicle.distance < nearbyDistance).slice(0, 5);
 
     res.json({ closestFiveVehicles }).end();
   });
--- server/usgs.js
+++ server/usgs.js
@@ -2,9 +2,15 @@ const request = require('request-promise');
 
 const USGS_HELPERS = require('./utils/usgs');
 
-const usgsURL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_day.geojson';
-// This is in meters
-const nearbyDistance = 300 * 1000;
+// All significant earthquakes from the day
+//  ("significant" definition: http://earthquake.usgs.gov/earthquakes/browse/significant.php#sigdef)
+// const usgsURL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_day.geojson';
+// All earthquakes M2.5+ from the past day
+// const usgsURL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson';
+// All earthquakes M1.0+ from the past day, useful for testing
+const usgsURL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_day.geojson';
+// This is in miles
+const nearbyDistance = 400;
 
 function getNearbyEarthquakes(req, res) {
   if (!req.body || !req.body.lat || !req.body.long) {
