05571a4505a869d54ee87613782ff9f93695db83
Ryan James <ryanwilsonjames@gmail.com>
2016-12-17 18:40:31 -0800

    Add server support for multiple earthquake sensitivity levels

--- server/usgs.js
+++ server/usgs.js
@@ -2,13 +2,14 @@ const request = require('request-promise');
 
 const USGS_HELPERS = require('./utils/usgs');
 
+const usgsURL = {};
 // All significant earthquakes from the day (this may be too strict)
 //  ("significant" definition: http://earthquake.usgs.gov/earthquakes/browse/significant.php#sigdef)
-// const usgsURL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_day.geojson';
+usgsURL.HIGH = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_day.geojson';
 // All earthquakes M2.5+ from the past day
-const usgsURL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson';
-// All earthquakes M1.0+ from the past day, useful for testing
-// const usgsURL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_day.geojson';
+usgsURL.MID = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson';
+// All earthquakes M1.0+ from the past day, useful for testing/"demo mode"
+usgsURL.LOW = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_day.geojson';
 
 // These determine what defines a "significant" quake.
 // It's all very subjective, so tweak till the results feel right.
@@ -31,18 +32,21 @@ function getNearbyEarthquakes(req, res) {
   if (!req.body || !req.body.lat || !req.body.long) {
     // TODO: early exit response (with error message?)
   }
-  const userLatLong = req.body;
+  const userLatLong = { lat: req.body.lat, long: req.body.long };
+  const sensitivityLevel = req.body.level || 'MID';
 
   request({
     method: 'GET',
-    uri: usgsURL,
+    uri: usgsURL[sensitivityLevel],
     gzip: true,
   })
   .then((response) => {
     const parsedJson = JSON.parse(response);
     const earthquakes = USGS_HELPERS.processUSGSResponse(parsedJson, userLatLong);
     const recentEarthquakes = earthquakes.filter((quake) => isRecent(quake));
-    const recentNearbyEarthquakes = recentEarthquakes.filter((quake) => isSignificant(quake));
+    const recentNearbyEarthquakes = sensitivityLevel === 'LOW' ?
+      earthquakes.filter((quake) => quake.distance < nearbyDistance)
+      : recentEarthquakes.filter((quake) => isSignificant(quake));
 
     res.json({ recentNearbyEarthquakes }).end();
   });
