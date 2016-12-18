506b0e678ca3848a9765e0e7d5471f520e113179
Ryan James <ryanwilsonjames@gmail.com>
2016-12-17 17:19:45 -0800

    Fix certs route typo

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
 app.post('/api/muni', muniHandler.getMuniData);
 app.post('/api/usgs', usgsHandler.getNearbyEarthquakes);
-app.get('.well-known/acme-challenge/FTsHz7fsFu_TDoK8m56U_zSRssK851DmfPgYh6xJbGk', certsHandler.acmeChallenge);
+app.get('/.well-known/acme-challenge/FTsHz7fsFu_TDoK8m56U_zSRssK851DmfPgYh6xJbGk', certsHandler.acmeChallenge);
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
