fe688101f04424c12c9cfcba707d8da4fd55b502
Ryan James <ryanwilsonjames@gmail.com>
2017-02-28 17:10:02 -0800

    more tests

--- src/views/location-warning.test.js
+++ src/views/location-warning.test.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import LocationWarningView from './location-warning';
+import LocationWarningView, { getWarningText } from './location-warning';
 import store from '../store';
 
 
+/*** component ***/
 const node = (
   <Provider store={store}>
     <LocationWarningView />
@@ -15,4 +16,17 @@ const node = (
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(node, div);
+});
+
+
+/*** getWarningText helper ***/
+// `expect.any` not currently supported in react-scripts 0.9.3?
+// it('accepts a string', () => {
+//   const mock = jest.fn();
+//   getWarningText(mock);
+//   expect(mock).toBeCalledWith(expect.any(String));
+// });
+
+it('returns a string', () => {
+  expect(typeof getWarningText()).toBe('string');
 });
