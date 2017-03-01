76a0a878ba1b66fb26c3565e435a5a9405b04119
Ryan James <ryanwilsonjames@gmail.com>
2017-02-28 18:17:47 -0800

    default action to empty object to avoid [undefined].type error

--- src/utils/create-reducer.js
+++ src/utils/create-reducer.js
@@ -1,7 +1,7 @@
 import { has } from 'lodash';
 
 export default function createReducer(initialState, handlers) {
-  return (state = initialState, action) => {
+  return (state = initialState, action = {}) => {
     if (has(handlers, action.type)) {
       return handlers[action.type](state, action);
     }
